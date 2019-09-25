2f382b18674c58dd01e848e5caaa8126f2e13081
github username <github email address>
2019-09-25 09:31:49 -0400

    Add mechanize gem

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,17 @@
 class Search < ApplicationRecord
+
+  validates :address1, presence: true
+  validates :restaurant, presence: true
+
+  after_initialize do |search|
+    scrapeGrubHub(search)
+  end
+
+  def scrapeGrubHub(search)
+    agent = Mechanize.new
+    page = agent.get('http://grubhub.com/')
+    pp page
+  end
+
+
 end
